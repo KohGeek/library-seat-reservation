a1b09dad6ffef1291054737139575a310793a30f
Jason07311 <jason07110731@gmail.com>
2022-03-29 01:24:25 +0800

    Create migrations
    Migration for users table, added with name,
    email,
    username,
    password,
    role(enum)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,12 +16,18 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['librarian','staff','student'])->default('student');
+
             $table->rememberToken();
             $table->timestamps();
         });
+
+        
+
     }
 
     /**
