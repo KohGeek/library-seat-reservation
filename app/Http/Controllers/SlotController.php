25b744a6ffbebac96ea4f3a8b684da1139127c6f
Jason07311 <jason07110731@gmail.com>
2022-04-14 14:58:54 +0800

    Minor changes on ViewSlot
    Removing extra codes
    Trying to modify variable for date and time

--- app/Http/Controllers/SlotController.php
+++ app/Http/Controllers/SlotController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\BookingData;
+use App\Models\Seat;
 
 class SlotController extends Controller
 {
--- resources/js/components/AdminLogs.js
+++ resources/js/components/AdminLogs.js
@@ -1,74 +1,79 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import { Table, Button, Modal, ModalHeader, ModalBody, ModalFooter, Input, FormGroup, Label, NavItem } from "reactstrap";
+import {
+    Table,
+    Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    Input,
+    FormGroup,
+    Label,
+    NavItem,
+} from "reactstrap";
 import axios from "axios";
 import dateFormat, { masks } from "dateformat";
 
 export default class AdminLogs extends Component {
-
     constructor() {
         super();
         this.state = {
             logs: [],
-            searchLogData: {seat:"", name:""},
+            searchLogData: { seat: "", name: "" },
             searchLogModal: false,
-            listseat:[]
-        }
+            listseat: [],
+        };
     }
 
     // Loads All Logs
     loadLog() {
         axios.get("http://127.0.0.1:80/api/adminlogs", {}).then((response) => {
             this.setState({
-                logs:response.data,
+                logs: response.data,
             });
         });
-        axios.get("http://127.0.0.1:80/api/adminlogs_listseat", {}).then((response) => {
-            this.setState({
-                listseat:response.data,
+        axios
+            .get("http://127.0.0.1:80/api/adminlogs_listseat", {})
+            .then((response) => {
+                this.setState({
+                    listseat: response.data,
+                });
             });
-        });
     }
 
     // Searching Logs
     searchLog() {
-        let {seat, name} = this.state.searchLogData
-        axios.get("http://127.0.0.1:80/api/adminlogs/search", {params: {seat, name}}).then((response) => {
-            this.setState({
-                logs:response.data,
-                searchLogData: {seat:"", name:""},
-                searchLogModal: false,
+        let { seat, name } = this.state.searchLogData;
+        axios
+            .get("http://127.0.0.1:80/api/adminlogs/search", {
+                params: { seat, name },
+            })
+            .then((response) => {
+                this.setState({
+                    logs: response.data,
+                    searchLogData: { seat: "", name: "" },
+                    searchLogModal: false,
+                });
             });
-        });
 
-        console.log(this.state.searchLogData)
+        console.log(this.state.searchLogData);
     }
-
-
-
-
-
-
-
 
     // Toggles
     toggleSearchLogModal() {
         this.setState({
-            searchLogModal:!this.state.searchLogModal
-        })
+            searchLogModal: !this.state.searchLogModal,
+        });
     }
-
 
     // DEFAULT STUFF
     componentWillMount() {
         this.loadLog();
     }
 
-
-
     render() {
         let logs = this.state.logs.map((log) => {
-
             // console.log(log.created_at);
             // console.log(log.date_time);
 
@@ -82,60 +87,92 @@ export default class AdminLogs extends Component {
                     <td>{log.purpose}</td>
                     <td>{log.seat}</td>
                     <td>{dateFormat(datetime_date, "yyyy-mm-dd HH:MM:ss")}</td>
-                    <td>{dateFormat(createdat_date, "UTC:yyyy-mm-dd HH:MM:ss")}</td>
+                    <td>
+                        {dateFormat(createdat_date, "UTC:yyyy-mm-dd HH:MM:ss")}
+                    </td>
                 </tr>
             );
         });
 
         let seats = this.state.listseat.map((seat) => {
-
             return (
-                <option key={seat.id}
-                    value = {seat.id}
-                    >{seat.id}</option>
+                <option key={seat.id} value={seat.id}>
+                    {seat.id}
+                </option>
             );
         });
 
-
-        return(
+        return (
             <div className="container">
-
                 {/* Searching Log Section */}
                 <div>
-                    <Button color="primary" onClick={this.toggleSearchLogModal.bind(this)}> {" "} Search Log {" "}</Button>
+                    <Button
+                        color="primary"
+                        onClick={this.toggleSearchLogModal.bind(this)}
+                    >
+                        {" "}
+                        Search Log{" "}
+                    </Button>
 
-                    <Modal isOpen={this.state.searchLogModal} toggle={this.toggleSearchLogModal.bind(this)}>
-                    <ModalHeader toggle={this.toggleSearchLogModal.bind(this)}> Search Log </ModalHeader>
-                    <ModalBody>
-                        <FormGroup>
-                            <Label > Person Name </Label>
-                            <Input id = "name"
-                                value = {this.state.searchLogData.name}
-                                onChange = {(e) => {
-                                    let {searchLogData} = this.state
-                                    searchLogData.name = e.target.value
-                                    this.setState({searchLogData})
-                                }}> </Input>
-                        </FormGroup>
-                        <FormGroup>
-                            <Label>Seat</Label> <br></br>
-                            <select for="seats" id="seats"
-                            onChange = {(e) => {
-                                let {searchLogData} = this.state
-                                searchLogData.seat = e.target.value
-                                this.setState({searchLogData})}}
+                    <Modal
+                        isOpen={this.state.searchLogModal}
+                        toggle={this.toggleSearchLogModal.bind(this)}
+                    >
+                        <ModalHeader
+                            toggle={this.toggleSearchLogModal.bind(this)}
+                        >
+                            {" "}
+                            Search Log{" "}
+                        </ModalHeader>
+                        <ModalBody>
+                            <FormGroup>
+                                <Label> Person Name </Label>
+                                <Input
+                                    id="name"
+                                    value={this.state.searchLogData.name}
+                                    onChange={(e) => {
+                                        let { searchLogData } = this.state;
+                                        searchLogData.name = e.target.value;
+                                        this.setState({ searchLogData });
+                                    }}
                                 >
-                                <option></option> 
-                                {seats} </select>
-                        </FormGroup>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button color="primary" onClick={this.searchLog.bind(this)}> Filter </Button>
-                        <Button color="secondary" onClick={this.toggleSearchLogModal.bind(this)}> Cancel </Button>
-                    </ModalFooter>
+                                    {" "}
+                                </Input>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>Seat</Label> <br></br>
+                                <select
+                                    for="seats"
+                                    id="seats"
+                                    onChange={(e) => {
+                                        let { searchLogData } = this.state;
+                                        searchLogData.seat = e.target.value;
+                                        this.setState({ searchLogData });
+                                    }}
+                                >
+                                    <option></option>
+                                    {seats}{" "}
+                                </select>
+                            </FormGroup>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button
+                                color="primary"
+                                onClick={this.searchLog.bind(this)}
+                            >
+                                {" "}
+                                Filter{" "}
+                            </Button>
+                            <Button
+                                color="secondary"
+                                onClick={this.toggleSearchLogModal.bind(this)}
+                            >
+                                {" "}
+                                Cancel{" "}
+                            </Button>
+                        </ModalFooter>
                     </Modal>
                 </div>
-
 
                 {/* Load Table */}
                 <div>
@@ -150,17 +187,13 @@ export default class AdminLogs extends Component {
                                 <th> Booked At </th>
                             </tr>
                         </thead>
-                        <tbody>
-                            {logs}
-                        </tbody>
+                        <tbody>{logs}</tbody>
                     </Table>
                 </div>
 
-
-
                 {/* {checkSearch} */}
             </div>
-        )
+        );
     }
 }
 
