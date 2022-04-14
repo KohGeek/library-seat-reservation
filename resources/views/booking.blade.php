41e4190ff13efc01a57b57574900e62ddaae1b85
kohgeek <KohComputer@gmail.com>
2022-04-15 05:09:00 +0800

    Commit what I forgot to add

---
+++ resources/views/booking.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header">{{ __('Search Booking Log') }}</div>
+
+                <div class="card-body">
+                    <div id="booking"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
